f7adcf9cd03eb85d33e97c3b52ad0b64b53be757
Jonathanfy <jonathankarl@hotmail.com>
2017-04-21 12:10:43 +0200

    I'm retarded

--- sql/migrations/20170407111859_world.sql
+++ sql/migrations/20170407111859_world.sql
@@ -1,2 +1,2 @@
-INSERT INTOmigrationsVALUES ('20170407111859');
+INSERT INTO migrations VALUES ('20170407111859');
 UPDATE creature_template SET mindmg = 60 , maxdmg = 70, ScriptName = 'npc_arcanite_dragonling' WHERE entry = 12473;
