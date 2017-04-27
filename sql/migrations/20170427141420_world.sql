6cd337e2ff26cb206f06a381d9eae309f7af3750
Jonathanfy <jonathankarl@hotmail.com>
2017-04-27 18:34:10 +0200

    higher numbers

--- sql/migrations/20170427141420_world.sql
+++ sql/migrations/20170427141420_world.sql
@@ -1,3 +1,3 @@
-INSERT INTO migrations VALUES ('20170427141420')
+INSERT INTO migrations VALUES ('20170427141420');
 
-UPDATE creature_template Set mindmg = 90, maxdmg = 140  WHERE entry = 15901
+UPDATE creature_template Set mindmg = 200, maxdmg = 250  WHERE entry = 15901;
