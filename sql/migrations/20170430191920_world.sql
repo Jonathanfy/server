7d7e44055e8d3b91fe810a41cabfe864d1ea3455
Jonathanfy <jonathankarl@hotmail.com>
2017-04-30 23:49:06 +0200

    sql name

--- sql/migrations/20170430191920_world.sql
+++ sql/migrations/20170430191920_world.sql
@@ -1,2 +1,2 @@
 INSERT INTO migrations VALUES ('20170430191920');
-UPDATE creature_template SET scriptName = 'npc_TimbermawAncestor' WHERE entry = 15720;
+UPDATE creature_template SET scriptName = 'npc_timbermaw_ancestor' WHERE entry = 15720;
